39bcee37c9add640a01f3fdce31ffa117f096dc9
Cassius Reynolds <cassius2828@gmail.com>
2023-07-24 21:47:04 -0700

    created the search ability with a test array of objects

--- random-quote-gen/src/components/SearchPage/SearchQuotes.js
+++ random-quote-gen/src/components/SearchPage/SearchQuotes.js
@@ -1,37 +1,58 @@
-import tachyons from 'tachyons';
-import { CardInfo } from './CardInfo';
-import '../../index.css'
-import { colors } from '../MainPage/Buttons';
-import { useSelector } from 'react-redux';
+import tachyons from "tachyons";
+import { CardInfo } from "./CardInfo";
+import "../../index.css";
+import { colors } from "../MainPage/Buttons";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { robots } from "./TestAPI";
 
-export const SearchQuotes = ({searchChange}) => {
+export const SearchQuotes = () => {
   const newColorState = useSelector((state) => state.color);
+  const [search, setSearch] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+  const searchQuotes = (searchValue) => {
+    setSearch(searchValue);
+    if (search !== "") {
+      const filteredData = robots.filter((item) => {
+        return Object.values(item)
+          .join(" ")
+          .toLocaleLowerCase()
+          .includes(search.toLocaleLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(robots);
+    }
+  };
 
   return (
     <div className="search-container tc">
       <input
         style={{
           color: colors[newColorState],
-          border: `solid 2px ${colors[newColorState]}`,
+          border: `solid 4px ${colors[newColorState]}`,
         }}
-        onChange={searchChange}
+        onChange={(e) => searchQuotes(e.target.value)}
         className="tc f6 dim ba bw2 ph3 pv2 mb2 dib"
         type="search"
         placeholder="search for quotes..."
       />
       <div className="card-containers">
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
-        <CardInfo />
+        {search.length > 0 ? (filteredResults.map((item) => {
+         return <CardInfo quote={item.quote} author={item.author}/>
+        })) : (robots.map((item) => {
+         return <CardInfo quote={item.quote} author={item.author}/>
+        }))
+        
+        
+        }
       </div>
     </div>
   );
-}
+};
+
+//* Will add <Link> to navigate card info, will bring us back to main screen with that quote selected
+//* in order to do this, that change will have to change the state as well. Action creators are the only way to
+//*change state in such a way right? So I will need to look into that
+//? OR, I could make it to where when a card is clicked all other cards disappear and this one grows??
+//?which ever concept is more feasable I will pursue
---
+++ random-quote-gen/src/components/SearchPage/TestAPI.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    author: "Leanne Graham",
+    quote: "Br fd saf dsaf dsa fet",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    author: "Ervin Howell",
+    quote: "Anton ds af dsaf dsa fdsaf ette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    author: "Clementine Bauch",
+    quote: "Sama fdsaf dsaf dsa fafdsafntha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 4,
+    author: "Patricia Lebsack",
+    quote: "Kar lorem   fsagdsafd fdsafa ianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: 5,
+    author: "Chelsey Dietrich",
+    quote: "Ka fd saf dsa fd sgdsa mren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+  {
+    id: 6,
+    author: "Mrs. Dennis Schulist",
+    quote: "Leopo fds f  f  ds f dsaf dsaldo_Corkery",
+    email: "Karley_Dach@jasper.info",
+  },
+  {
+    id: 7,
+    author: "Kurtis Weissnat",
+    quote: "Elwy  fdsfdsgds gsd f ds fssfn.Skiles",
+    email: "Telly.Hoeger@billy.biz",
+  },
+  {
+    id: 8,
+    author: "Nicholas Runolfsdottir V",
+    quote: "Maxime fdsgggfs asd f dsf dsf s_Nienow",
+    email: "Sherwood@rosamond.me",
+  },
+  {
+    id: 9,
+    author: "Glenna Reichert",
+    quote: "Delph dfsa dsa fds af dsaf ds a ghrefaine",
+    email: "Chaim_McDermott@dana.io",
+  },
+  {
+    id: 10,
+    author: "Clementina DuBuque",
+    quote: "Moriah.Stanfdsa fdsafds fdsa fdsaf dsaton",
+    email: "Rey.Padberg@karina.biz",
+  },
+];
